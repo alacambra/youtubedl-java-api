231262f7d4563528dff4d389296d65c62fabcd3b
Albert Lacambra Basil <albert@lacambra.tech>
2019-01-08 20:03:57 +0100

    script updated

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export VERSION=1.0-SNAPSHOT
+export VERSION=$1
+echo "Download artrifacts for version ${VERSION}"
 ${DIR}/download.sh
+echo
 docker build -t lacambra/downloader:${VERSION} ${DIR}/docker/
+docker stop downloader
+docker rm downloader
+docker run -d --name downloader lacambra/downloader:${VERSION}
--- jenkins/scripts/download.sh
+++ jenkins/scripts/download.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 N=1.0
-VERSION=1.0-SNAPSHOT
+
+
+if [ -z ${VERSION} ];
+then
+VERSION=$1
+fi
+echo "Downloading version ${VERSION}";
 TS=$(curl https://jfrog.lacambra.tech/artifactory/list/lacambra.tech/tech/lacambra/downloader/downloader-server/${VERSION}/maven-metadata.xml | xmlstarlet sel -t -v '/metadata/versioning/snapshotVersions/snapshotVersion[1]/value')
 URL="https://jfrog.lacambra.tech/artifactory/list/lacambra.tech/tech/lacambra/downloader/downloader-server/${VERSION}/downloader-server-${TS}.war"
 echo ${URL}
