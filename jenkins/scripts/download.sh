b18b729cf04375f4bc86e3093c0c60aa84b490c8
Albert Lacambra Basil <albert@lacambra.tech>
2019-01-08 20:26:20 +0100

    script updated

--- jenkins/scripts/download.sh
+++ jenkins/scripts/download.sh
@@ -10,4 +10,5 @@ echo "Downloading version ${VERSION}";
 TS=$(curl https://jfrog.lacambra.tech/artifactory/list/lacambra.tech/tech/lacambra/downloader/downloader-server/${VERSION}/maven-metadata.xml | xmlstarlet sel -t -v '/metadata/versioning/snapshotVersions/snapshotVersion[1]/value')
 URL="https://jfrog.lacambra.tech/artifactory/list/lacambra.tech/tech/lacambra/downloader/downloader-server/${VERSION}/downloader-server-${TS}.war"
 echo ${URL}
+rm docker/*.war
 curl  ${URL} -o docker/downloader.war
