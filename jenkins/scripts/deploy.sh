b853849db0485d8f98708ebccb78b4e255061b7d
Albert Lacambra <alacambra@gmail.com>
2019-07-28 20:44:53 +0200

    restart container

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -7,4 +7,4 @@ echo
 docker build -t lacambra/downloader:${VERSION} ${DIR}/docker/
 docker stop downloader
 docker rm downloader
-docker run -d --name downloader -v ${DOWNLOADER_DOCKER_CONFIG_PATH}:/opt/downloder/config/ -p 28080:8080 -p 28443:8443 lacambra/downloader:${VERSION}
+docker run --restart unless-stopped -d --name downloader -v ${DOWNLOADER_DOCKER_CONFIG_PATH}:/opt/downloder/config/ -p 28080:8080 -p 28443:8443 lacambra/downloader:${VERSION}
